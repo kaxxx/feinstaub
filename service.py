5b4136810b8cd3b1f7527de1b3b17c2fd0c4779f
kaxxx <kay.berkemeier@googlemail.com>
2019-11-08 18:13:21 +0100

    template folder defined debug mode true

--- service.py
+++ service.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python2.7 -u
 # coding=utf-8
-from flask import Flask, jsonify, json
+
+from flask import Flask, jsonify, json, render_template
 from fsdb import Fsdb
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates', static_folder='./static')
 
 tasks = [
     {
@@ -20,14 +21,22 @@ tasks = [
     }
 ]
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/lastdays/<days>")
+def lastdays(days):
+    return render_template("index.html", days=days)
+
 @app.route('/fs/api/v1.0/lastdays/<days>', methods=['GET'])
 def get_tasks(days):
     db = Fsdb()
     return jsonify(db.getLastDays(days))
-    #return json.dumps();
+    #return json.dumps(db.getLastDays(days));
 
 def get_data():
     return
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0')
+    app.run(debug=True, host= '0.0.0.0')
