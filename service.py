69d0c95acd8b70ff057cf6e4525ce946c681c345
kaxxx <kay.berkemeier@googlemail.com>
2019-11-08 18:20:12 +0100

    deleted unused code

--- service.py
+++ service.py
@@ -6,21 +6,6 @@ from fsdb import Fsdb
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
 @app.route("/")
 def home():
     return render_template("index.html")
